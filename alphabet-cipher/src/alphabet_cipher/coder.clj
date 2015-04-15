62020234d7c7c47dd0b4ba8b7d8b8e0a52e1aad1
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-04-15 17:01:18 +0100

    solved alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,23 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet (map char (range 97 123)))
+
+(defn get-char-idx [char]
+  (.indexOf alphabet char))
+
+(defn construct-keyword [keyword message]
+  (take (count message) (cycle keyword)))
+
+(defn encode-char [key-char msg-char]
+  (let [offset (mod (+ (get-char-idx msg-char) (get-char-idx key-char)) 26)]
+    (nth alphabet offset)))
+
+(defn decode-char [key-char msg-char]
+  (let [offset (mod (- (get-char-idx msg-char) (get-char-idx key-char)) 26)]
+    (nth alphabet offset)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (reduce str (map encode-char (construct-keyword keyword message) (seq message))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (reduce str (map decode-char (construct-keyword keyword message) (seq message))))
