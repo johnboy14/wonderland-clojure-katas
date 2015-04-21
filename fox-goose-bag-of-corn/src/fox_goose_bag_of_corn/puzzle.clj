d00ea2b37e444594bb38bab035aea0d1f4753ae8
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-04-21 16:08:27 +0100

    first commit of solution.  Not complete

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,32 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
-(defn river-crossing-plan []
-  start-pos)
+(defn finished? [[left-bank boat [crossing-plan]]]
+  (and (empty? left-bank) (= [:boat] boat)))
+
+(defn fox-alone-with-goose? [])
+
+(defn goose-alone-with-corn? [])
+
+(defn has-driver? [boat]
+  (pos? (.indexOf boat :you)))
+
+(defn not-driver? [passenger]
+  (false? (= passenger :you)))
+
+(defn add-driver [crossing-plan]
+  (conj crossing-plan [(filterv not-driver? (first crossing-plan))
+                       (conj (nth crossing-plan 1) :you)
+                       (last crossing-plan)]))
+
+(defn add-passenger [crossing-plan]
+  (if-not (has-driver? (nth crossing-plan 1))
+    (add-passenger (add-driver crossing-plan))))
+
+(defn river-crossing-plan [] 
+  (loop [river-crossing-plan start-pos]
+    (if (finished? (last river-crossing-plan))
+      river-crossing-plan
+      (recur (conj river-crossing-plan (add-passenger (last river-crossing-plan)))))))
+
+;(river-crossing-plan)
