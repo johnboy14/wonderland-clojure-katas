62b25a55f289f00167d670915fadbeba158bad3c
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-04-22 12:12:28 +0100

    Adding passengers done

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -5,29 +5,48 @@
 (defn finished? [[left-bank boat [crossing-plan]]]
   (and (empty? left-bank) (= [:boat] boat)))
 
-(defn fox-alone-with-goose? [])
-
-(defn goose-alone-with-corn? [])
+(defn remove-passenger [plan passenger]
+  (vec (remove #{passenger} plan)))
 
 (defn has-driver? [boat]
-  (pos? (.indexOf boat :you)))
+  (> (.indexOf boat :you) -1))
 
-(defn not-driver? [passenger]
-  (false? (= passenger :you)))
+(defn fox-and-goose-together? [passengers]
+  (and (> (.indexOf passengers :fox) -1)
+       (> (.indexOf passengers :goose) -1)
+       (= (count passengers) 2)))
 
-(defn add-driver [crossing-plan]
-  (conj crossing-plan [(filterv not-driver? (first crossing-plan))
-                       (conj (nth crossing-plan 1) :you)
-                       (last crossing-plan)]))
+(defn goose-and-corn-together? [passengers]
+  (and (> (.indexOf passengers :corn) -1)
+       (> (.indexOf passengers :goose) -1)
+       (= (count passengers) 2)))
+
+(defn board-boat [crossing-plan passenger]
+  (conj crossing-plan
+        (vec [(remove-passenger (first (last crossing-plan)) passenger)
+              (conj (nth (last crossing-plan) 1) passenger)
+              (last (last crossing-plan))])))
+
+(defn next-passenger [remaining-passengers]
+  (first (filterv #(false? (or (fox-and-goose-together? (remove-passenger remaining-passengers %))
+                         (goose-and-corn-together? (remove-passenger remaining-passengers %))))
+            remaining-passengers)))
 
 (defn add-passenger [crossing-plan]
-  (if-not (has-driver? (nth crossing-plan 1))
-    (add-passenger (add-driver crossing-plan))))
+  (if-not (has-driver? (nth (last crossing-plan) 1))
+    (add-passenger (board-boat crossing-plan :you))
+    (board-boat crossing-plan (next-passenger (first (last crossing-plan))))))
 
-(defn river-crossing-plan [] 
-  (loop [river-crossing-plan start-pos]
-    (if (finished? (last river-crossing-plan))
-      river-crossing-plan
-      (recur (conj river-crossing-plan (add-passenger (last river-crossing-plan)))))))
+(add-passenger (add-passenger start-pos))
 
-;(river-crossing-plan)
+(defn dissembark [crossing-plan]
+  )
+
+(defn river-crossing-plan []
+  (loop [crossing-plan start-pos]
+    (if-not (finished? (last crossing-plan))
+      (-> crossing-plan
+          add-passenger
+          dissembark
+          recur)
+      crossing-plan)))
