a3560a447b7c3065396dae73671ec4f52e75203f
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-04-28 13:46:54 +0100

    initial commit

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -2,10 +2,19 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
+
+(def alphabet (map char (range 97 123)))
+
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
+(defn wrap-alphabet-from [letter]
+  (let [ascii (int letter)
+        first-half (mapv char (range 97 ascii))
+        second-half (mapv char (range (inc ascii) 123))]
+    (into [] (concat first-half second-half))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  )
